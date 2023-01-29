7fac60d09517751aa41720ade5ee74fc594ccdbf
Utkarsh Kesarwani <41234749+utkarsh-kesarwaNi@users.noreply.github.com>
2023-01-29 12:49:37 +0000

    cpp template

---
+++ Template.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+#define FOR(i,n) for(int i=0;i<n;i++)
+#define PI 3.141592653589793238462
+#define debug(x) cout << #x << " " << x <<endl;
+#define pb push_back
+#define nline "\n"
+typedef long long ll;
+
+using namespace std;
+
+void solve(){
+}
+
+int main(){
+        solve();
+        return 0;
+}

e491afaaf044e46c571efde0464d90a55d65ce33
Utkarsh Kesarwani <>
2023-04-06 12:51:37 +0000

    O(n) solution added

--- BinarySearch/peakIndexInMountainArray.cpp
+++ BinarySearch/peakIndexInMountainArray.cpp
@@ -2,6 +2,27 @@
 #include <vector>
 using namespace std;
 
+// Brute force
+// TC=O(N)
+int getPeakElement(vector<int> &nums) {
+  if (nums.size() == 1) {
+    return 0;
+  }
+  for (int i = 0; i < nums.size(); i++) {
+    if (i == 0 && nums[0] > nums[1]) {
+      return 0;
+    }
+    if (i == nums.size() - 1 && nums[i] > nums[i - 1]) {
+      return nums.size() - 1;
+    }
+    if (nums[i] > nums[i + 1] && nums[i] > nums[i - 1]) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+// Optimized time complexity using Binary Search
 int peakIndexInMountainArray(vector<int> &arr) {
   int start = 0, end = arr.size() - 1;
   int mid = start + (end - start) / 2;
